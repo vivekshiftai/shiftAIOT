18f0612417981b1f6f3580e03cad75d7e8d48c5b
vivekshiftai <s.tadikonda@theshiftai.in>
2025-09-17 12:55:54 +0530

    added clor changes in sales

--- src/components/SalesIntelligence/CustomerRecommendationsDisplay.tsx
+++ src/components/SalesIntelligence/CustomerRecommendationsDisplay.tsx
@@ -240,7 +240,7 @@ export const CustomerRecommendationsDisplay: React.FC<CustomerRecommendationsDis
         </div>
 
         {/* Tab Content */}
-        <div className="p-4 bg-gray-100">
+        <div className="p-4 bg-white">
           <div className="space-y-6 bg-white rounded-lg border border-gray-200">
             {getRecommendationsForTab().length === 0 ? (
               <div className="text-center py-8 bg-white rounded-lg border border-gray-200">
